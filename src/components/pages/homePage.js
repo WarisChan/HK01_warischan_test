098404482093551f33b1bb3343cc226fdd10400f
warischan <kawai0522hk2006@hotmail.com>
2017-11-14 17:09:42 +0800

    submit version v3

--- src/components/pages/homePage.js
+++ src/components/pages/homePage.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import _ from 'lodash';
+import { isChrome, isFirefox, isSafari, isIE, isEdge, isOpera } from './BrowserDetection'
 
 const propTypes = {
     items: PropTypes.array.isRequired,
@@ -11,9 +12,12 @@ const propTypes = {
 const defaultProps = {
     initialPage: 1
 }
+
+let isOnComposition = false;
+let isInnerChangeFromOnChange = false;
 	
 class Pagination  extends Component {
-	 constructor(props) {
+	constructor(props) {
         super(props);
         this.state = { pager: {} };
     }
@@ -35,10 +39,6 @@ class Pagination  extends Component {
     setPage(page) {
         var items = this.props.items;
         var pager = this.state.pager;
-
-        if (page < 1 || page > pager.totalPages) {
-            return;
-        }
 
         // get new pager object for specified page
         pager = this.getPager(items.length, page);
@@ -126,13 +126,15 @@ class Pagination  extends Component {
 }
 
 class Homepage extends React.Component {
-	constructor() {
-        super();
+	constructor(props) {
+        super(props);
 		
         this.state = {
             topfreeapp: [],
             grossingapp: [],
             pageOfItems: [],
+            topfreeapp_filter: [],
+            grossingapp_filter: [],
 			typed: "",
 			page: 0
         };
@@ -167,7 +169,9 @@ class Homepage extends React.Component {
 		});
     }
 
-	updateSearch(event) {
+	updateSearch(event) {		
+		event.target.value = event.target.value.replace(/\\/g, "\\\\");
+		
 		this.setState({ 
 			typed: event.target.value 
 		});
@@ -179,7 +183,6 @@ class Homepage extends React.Component {
 				return item["im:name"].label.toLowerCase().search(
 				searchword.toLowerCase()) !== -1;
 			});
-			
 			return updatedList;
 		}
 		
@@ -191,14 +194,65 @@ class Homepage extends React.Component {
 			});
 		});
 		
-		
 		fetch("https://itunes.apple.com/hk/rss/topgrossingapplications/limit=10/json")
 		.then( (response) => response.json() )   
 		.then( (json) => {
 			this.setState({
 				grossingapp: addlist(json.feed.entry, this.state.typed)
 			});
-		});		
+		});
+	}
+	
+	handleComposition = (e: Event) => {
+		if (!(e.target instanceof HTMLInputElement)) return
+
+		if (e.type === 'compositionend') {
+			// Chrome is ok for only setState innerValue
+			// Opera, IE and Edge is like Chrome
+			if (isChrome || isIE || isEdge || isOpera) {
+				this.setState({ innerValue: e.target.value })
+			}
+
+			// Firefox need to setState inputValue again...
+			if (isFirefox) {
+				this.setState({ innerValue: e.target.value, inputValue: e.target.value })
+			}
+
+			// Safari think e.target.value in composition event is keyboard char,
+			//  but it will fired another change after compositionend
+			if (isSafari) {
+				 // do change in the next change event
+				isInnerChangeFromOnChange = true
+			}
+			
+			this.updateSearch(e);
+
+			isOnComposition = false
+		} else {
+			isOnComposition = true
+		}
+	}
+
+	handleChange = (e: Event) => {
+		if (!(e.target instanceof HTMLInputElement)) return
+
+		if (isInnerChangeFromOnChange) {
+			this.setState({ inputValue: e.target.value, innerValue: e.target.value })
+			isInnerChangeFromOnChange = false;
+			return
+		}
+
+		// when is on composition, change inputValue only
+		// when not in composition change inputValue and innerValue both
+		if (!isOnComposition) {
+			this.setState({
+				inputValue: e.target.value,
+				innerValue: e.target.value,
+			})
+			this.updateSearch(e);
+		} else {
+			this.setState({ inputValue: e.target.value })
+		}
 	}
 		
 	render() {
@@ -207,7 +261,7 @@ class Homepage extends React.Component {
 			<div>
 				<div className="right-inner-addon text-center">
 					<div className="search-container">
-						<input className="glyphicon" type="text" onChange={this.updateSearch.bind(this)} placeholder="&#xe003;搜尋"/>
+						<input className="glyphicon" type="text" onCompositionStart={this.handleComposition}  onCompositionUpdate={this.handleComposition} onCompositionEnd={this.handleComposition} onChange={this.handleChange} placeholder="&#xe003;搜尋"/>
 					</div>
 				</div>
 				<div className="container">
