f49248cf98b19358e81fd4cc0bd1bf34d117d4b9
warischan <kawai0522hk2006@hotmail.com>
2017-11-14 13:30:31 +0800

    submit version

--- src/App.js
+++ src/App.js
@@ -1,30 +1,22 @@
 import React, { Component } from 'react';
-import{
-	BrowserRouter as Router,
-	Route
-} from 'react-router-dom'
 
 import './Assets/css/default.min.css';
 
 import Footer from './components/footerComponents/footer';
 import Homepage from './components/pages/homePage';
-import Products from './components/pages/products';
 
 class App extends Component {
   render() {
     return (
-		<Router>
-			<div className="App">
-	  
-			
-			
-				<Route exact path='/' component={Homepage} />
-				<Route exact path='/Products' component={Products} />
-			
-			<Footer />
+		<div className="App">
+  
 		
-			</div>
-		</Router>
+		
+			<Homepage />
+		
+		<Footer />
+	
+		</div>
     );
   }
 }
--- src/components/headerComponents/header.js
+++ src/components/headerComponents/header.js
@@ -4,7 +4,7 @@ class Header extends Component {
   render() {
     return (
 		<header>
-			<div className="logo">LOGO</div>
+			
 		</header>
     );
   }
